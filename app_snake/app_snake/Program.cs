f8f2f33d6874a1aaeee67def84778f4ca8c98403
Lyutyiy <lytuy03115@gmail.com>
2017-01-22 21:51:49 +0200

    Create class Point

--- app_snake/app_snake/Program.cs
+++ app_snake/app_snake/Program.cs
@@ -10,28 +10,19 @@ namespace app_snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sum1 = '*';
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
 
-            int x2 = 4;
-            int y2 = 5;
-            char sum2 = '#';
-
-            Draw(x1, y1, sum1);
-
-            Draw(x2, y2, sum2);
-
-
-
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw();
 
             Console.ReadLine();
-        }
-
-        static void Draw(int x, int y, char sum)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sum);
         }
     }
 }
